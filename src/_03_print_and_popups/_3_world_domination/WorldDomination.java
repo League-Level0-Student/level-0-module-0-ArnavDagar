d2ab4ddf92af53eed8bd0c071f7426881e22a921
root <root@5f8ae2057ace>
2020-06-15 23:53:45 +0000

    Completed Greeter Completed Madlibs Completed Quiz Completed World Domination

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -7,9 +7,14 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		
-		// 1. Ask the user if they know how to write code.
+		String input = JOptionPane.showInputDialog("Do you knonw how to code?");
+		
+		if(input.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world");
 
-		// 2. If they say "yes", tell them they will rule the world.
+		} else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes at The League");
+		}
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,14 +1,45 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int count = 0;		
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
-				// 2.  Ask the user a question 
-				
+String input = JOptionPane.showInputDialog("What year was Microsoft established?");
+		
+		if(input.equals("1975")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+			count = count + 1;
+
+		} else {
+			JOptionPane.showMessageDialog(null, "WRONG!");
+			count = count - 1;
+		}
+
+String input1 = JOptionPane.showInputDialog("How many sides does a hexagon have?");
+		
+		if(input1.equals("6")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+			count = count + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "WRONG!");
+			count = count -1;
+		}
+
+String input11 = JOptionPane.showInputDialog("How many meters are there in a kilometer");
+		
+		if(input11.equals("100")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+			count = count + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes at The League");
+			count = count - 1:
+		}
+
+		
 				// 3.  Use an if statement to check if their answer is correct
 				
 				// 4.  if the user's answer was correct, add one to their score 
